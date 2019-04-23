cf8b686c699a263ca2589ecf0742a26a91e43718
Kamohelo <kamzapercy@gmail.com>
2019-04-23 13:04:06 +0200

    second

---
+++ App2/Database.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+using System.Data;
+using SQLite;
+using App2.Resources.Model;
+namespace App2.Resources.Helper
+{
+    class Database
+    {
+        string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        public bool createDatabase()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Shopping.db")))
+                {
+                    connection.CreateTable<Shopping>();
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+        //Add or Insert Operation  
+
+        public bool insertIntoTable(Shopping shopping)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Shopping.db")))
+                {
+                    connection.Insert(shopping);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+        public List<Shopping> selectTable()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Shopping.db")))
+                {
+                    return connection.Table<Shopping>().ToList();
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return null;
+            }
+        }
+        //Edit Operation  
+
+        public bool updateTable(Shopping shopping)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Shopping.db")))
+                {
+                    connection.Query<Shopping>("UPDATE Shopping set Name=?, Where Id=?", shopping.Name, shopping.Id);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+        //Delete Data Operation  
+
+        public bool removeTable(Shopping shopping)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Shopping.db")))
+                {
+                    connection.Delete(shopping);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+        //Select Operation  
+
+        public bool selectTable(int Id)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Shopping.db")))
+                {
+                    connection.Query<Shopping>("SELECT * FROM Shopping Where Id=?", Id);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ App2/MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using App2.Resources.Helper;
+using App2.Resources.Model;
+using System.Collections.Generic;
+namespace App2
+{
+    [Activity(Label = "App2", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        ListView lstViewData;
+        List<Shopping> listSource = new List<Shopping>();
+        Database db;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            //Create Database  
+            db = new Database();
+            db.createDatabase();
+            lstViewData = FindViewById<ListView>(Resource.Id.listView);
+            var edtName = FindViewById<EditText>(Resource.Id.edtName);
+            var btnAdd = FindViewById<Button>(Resource.Id.btnAdd);
+            var btnEdit = FindViewById<Button>(Resource.Id.btnEdit);
+            var btnRemove = FindViewById<Button>(Resource.Id.btnRemove);
+            //Load Data  
+            LoadData();
+            //Event  
+            btnAdd.Click += delegate
+            {
+                Shopping shopping = new Shopping()
+                {
+                    Name = edtName.Text,                   
+                };
+                db.insertIntoTable(shopping);
+                LoadData();
+            };
+            btnEdit.Click += delegate
+            {
+                Shopping shopping = new Shopping()
+                {
+                    Id = int.Parse(edtName.Tag.ToString()),
+                    Name = edtName.Text,                   
+                };
+                db.updateTable(shopping);
+                LoadData();
+            };
+            btnRemove.Click += delegate
+            {
+                Shopping shopping = new Shopping()
+                {
+                    Id = int.Parse(edtName.Tag.ToString()),
+                    Name = edtName.Text,                  
+                };
+                db.removeTable(shopping);
+                LoadData();
+            };
+            lstViewData.ItemClick += (s, e) =>
+            {
+                //Set Backround for selected item  
+                for (int i = 0; i < lstViewData.Count; i++)
+                {
+                    if (e.Position == i)
+                        lstViewData.GetChildAt(i).SetBackgroundColor(Android.Graphics.Color.Black);
+                    else
+                        lstViewData.GetChildAt(i).SetBackgroundColor(Android.Graphics.Color.Transparent);
+                }
+                //Binding Data  
+                var txtName = e.View.FindViewById<TextView>(Resource.Id.txtView_Name);                          
+                edtName.Tag = e.Id;            
+            };
+        }
+        private void LoadData()
+        {
+            listSource = db.selectTable();
+            var adapter = new ListViewAdapter(this, listSource);
+            lstViewData.Adapter = adapter;
+        }
+    }
+}  
